90bfb7f57b1d45f57c00f963af05be57d594e500
Paul Lewis <paularthurlewis@gmail.com>
2014-06-25 08:42:22 -0700

    Added GameFragment

---
+++ Abstract Games/src/main/java/com/castlefrog/games/asg/GameFragment.java
@@ -0,0 +1,15 @@
+package com.castlefrog.games.asg;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+}
