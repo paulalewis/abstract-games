fdc153b7ad0051f5d33aaa224f5f6f1f2ac398cf
Paul Lewis <paularthurlewis@gmail.com>
2015-03-22 19:44:10 -0700

    Added help and update menu

--- Abstract Games/src/main/kotlin/com/castlefrog/games/asg/GameFragment.kt
+++ Abstract Games/src/main/kotlin/com/castlefrog/games/asg/GameFragment.kt
@@ -2,11 +2,10 @@ package com.castlefrog.games.asg
 
 import android.app.ActionBar
 import android.app.Fragment
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.castlefrog.agl.Agent
 import com.castlefrog.agl.Arbiter
 import java.util.concurrent.Executors
@@ -28,15 +27,16 @@ public class GameFragment : Fragment() {
     private val executor = Executors.newSingleThreadExecutor()
     private val arbiter: Arbiter<*, *>? = null
     private val agents: List<Agent>? = null
-    private var helpUri: Uri? = null
     private var gameType = ""
+    private var helpUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bundle = savedInstanceState ?: getArguments()
         gameType = bundle.getString(ARG_GAME_TYPE)
-        //val resId = getResources().getIdentifier("help_uri_" + gameType, "string", getActivity().getPackageName())
-        //helpUri = Uri.parse(getString(resId))
+        val resId = getResources().getIdentifier("help_uri_" + gameType.toLowerCase(), "string", getActivity().getPackageName())
+        helpUri = Uri.parse(getString(resId))
+        setHasOptionsMenu(true)
     }
 
     override fun onResume() {
@@ -45,7 +45,28 @@ public class GameFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        val view = inflater.inflate(R.layout.fragment_game, container, false)
+        return view
+    }
+
+    override public fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.game, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override public fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            R.id.action_settings -> {
+                true
+            }
+            R.id.action_about -> {
+                startActivity(Intent(Intent.ACTION_VIEW, helpUri))
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item);
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
