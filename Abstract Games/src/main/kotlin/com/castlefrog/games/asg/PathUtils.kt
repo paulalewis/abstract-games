6887d8e7290b587c631cd1ba5b1983804a652c57
Paul Lewis <paularthurlewis@gmail.com>
2015-04-20 21:29:56 -0700

    Convert PathUtils to kotlin

---
+++ Abstract Games/src/main/kotlin/com/castlefrog/games/asg/PathUtils.kt
@@ -0,0 +1,43 @@
+package com.castlefrog.games.asg
+
+import android.graphics.Matrix
+import android.graphics.Path
+
+public class PathUtils {
+    companion object {
+        public val HEXAGON_RADIUS: Float = 1f
+        public val HEXAGON_HALF_RADIUS: Float = HEXAGON_RADIUS * 0.5f
+        public val HEXAGON_SHORT_RADIUS: Float = HEXAGON_HALF_RADIUS * Math.sqrt(3.0).toFloat()
+
+        /**
+         * @param radius of the circumscribed circle
+         * @return a Path object in the shape of a hexagon
+         */
+        public fun getHexagon(radius: Float): Path {
+            return getRegularPolygon(6, radius)
+        }
+
+        /**
+         * Get a path in the shape of a regular polygon.
+         * @param nSides number of sides to polygon
+         * @param radius radius of the circumscribed circle
+         * @return a Path object in the shape of a regular polygon
+         */
+        public fun getRegularPolygon(nSides: Int, radius: Float): Path {
+            val path = Path()
+            if (nSides < 3) {
+                return path
+            }
+            val a = (Math.PI * 2) / nSides
+            path.moveTo(radius, 0f)
+            for (i in 1..nSides - 1) {
+                path.lineTo((radius * Math.cos(a * i)).toFloat(), (radius * Math.sin(a * i)).toFloat())
+            }
+            path.close()
+            val matrix = Matrix()
+            matrix.postTranslate(radius, radius)
+            path.transform(matrix)
+            return path
+        }
+    }
+}
