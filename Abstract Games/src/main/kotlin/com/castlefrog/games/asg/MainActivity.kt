1b3d0dcba5d187b134417eec9bb14f7783c0e076
Paul Lewis <paularthurlewis@gmail.com>
2016-05-25 22:25:11 -0700

    update add icon

--- Abstract Games/src/main/kotlin/com/castlefrog/games/asg/MainActivity.kt
+++ Abstract Games/src/main/kotlin/com/castlefrog/games/asg/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.castlefrog.games.asg
 
 import android.app.Activity
-import android.graphics.drawable.PictureDrawable
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ImageView
-import com.caverock.androidsvg.SVG
 
 class MainActivity : Activity() {
 
@@ -18,7 +14,6 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val add = findViewById(R.id.add) as FloatingActionButton
-        add.setImageSvg(R.raw.ic_add)
         add.setOnClickListener {
             SelectGameTypeFragment().show(fragmentManager, null)
         }
@@ -37,11 +32,5 @@ class MainActivity : Activity() {
             return true
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    fun ImageView.setImageSvg(resId: Int) {
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        val svg = SVG.getFromResource(resources, resId)
-        setImageDrawable(PictureDrawable(svg.renderToPicture()))
     }
 }
