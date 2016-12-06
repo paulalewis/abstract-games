d3eed995905c5559bab993206bbaba432ba958b5
Paul Lewis <paularthurlewis@gmail.com>
2016-12-05 21:35:20 -0800

    game activity uses anko

--- Abstract Games/src/main/kotlin/com/castlefrog/games/asg/GameActivity.kt
+++ Abstract Games/src/main/kotlin/com/castlefrog/games/asg/GameActivity.kt
@@ -4,7 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.castlefrog.games.asg.havannah.HavannahFragment
+import com.castlefrog.games.asg.hex.HexFragment
+import com.castlefrog.games.asg.model.DomainType
 import com.castlefrog.games.asg.model.Game
+import org.jetbrains.anko.*
 
 class GameActivity : Activity() {
 
@@ -20,9 +24,18 @@ class GameActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
-        val game : Game = intent.extras.getSerializable(ARG_GAME) as Game
-        val fragment = GameFragment.newInstance(game)
+        frameLayout {
+            id = R.id.container
+            horizontalPadding = resources.getDimensionPixelSize(R.dimen.activity_horizontal_margin)
+            verticalPadding = resources.getDimensionPixelSize(R.dimen.activity_vertical_margin)
+            background = context.getDrawable(android.R.color.black)
+        }
+        val extras = savedInstanceState ?: intent.extras
+        val game : Game = extras.getSerializable(ARG_GAME) as Game
+        val fragment = when (game.domain.type) {
+            DomainType.HEX -> HexFragment.newInstance(game)
+            DomainType.HAVANNAH -> HavannahFragment.newInstance(game)
+        }
         fragmentManager.beginTransaction()
                 .replace(R.id.container, fragment, null)
                 .commit()
