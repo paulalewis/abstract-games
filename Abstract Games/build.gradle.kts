607dac2dd2b1c428078795c9e343ddc411500120
Paul Lewis-Parker <paularthurlewis@gmail.com>
2024-08-21 23:30:52 -0700

    remove top bar

--- Abstract Games/build.gradle.kts
+++ Abstract Games/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            // isMinifyEnabled = false
         }
     }
     buildFeatures {
@@ -38,37 +38,41 @@ android {
 }
 
 dependencies {
-    val compose = "1.6.8"
-    val retrofit = "2.8.1"
-    val moshi = "1.9.2"
-    val kotlin = "1.9.20"
-    val room = "2.6.1"
-
     implementation(files("libs/kotlin-age-0.3.0.jar"))
-    implementation(platform("com.google.firebase:firebase-bom:30.0.1"))
 
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("io.arrow-kt:arrow-core:1.2.1")
-    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2") // todo remove
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
-    implementation("androidx.compose.ui:ui:$compose")
-    implementation("androidx.compose.material:material:$compose")
-    implementation("androidx.compose.ui:ui-tooling:$compose")
+
+    val kotlin = "1.9.20"
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin")
+
+    implementation(platform("com.google.firebase:firebase-bom:30.0.1"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.firebaseui:firebase-ui-auth:8.0.1")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin")
-    implementation("com.squareup.moshi:moshi:$moshi")
-    implementation("com.squareup.moshi:moshi-kotlin:$moshi")
-    implementation("com.squareup.retrofit2:retrofit:$retrofit")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
+
+    val compose = "1.6.8"
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.compose.ui:ui:$compose")
+    implementation("androidx.compose.material:material:$compose")
+    implementation("androidx.compose.ui:ui-tooling:$compose")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
 
+    implementation("io.arrow-kt:arrow-core:1.2.1")
+
+    val moshi = "1.9.2"
+    implementation("com.squareup.moshi:moshi:$moshi")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshi")
+
+    val retrofit = "2.8.1"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
+
+    val room = "2.6.1"
     implementation("androidx.room:room-runtime:$room")
     annotationProcessor("androidx.room:room-compiler:$room")
     ksp("androidx.room:room-compiler:$room")
